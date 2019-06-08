8ae727af0165e07eea9334cf4e7e1a34a0b5c595
Aluno <>
2019-06-07 22:35:38 -0300

    criando as classses

--- barbearia/agendamento/models.py
+++ barbearia/agendamento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class  Estado(models.Model):
+    sigla = models.CharField(max_lenght=2)
+    nome = models.CharField(max_lenght=50 )
+
+    def __str__(self):
+        return self.sigla + ' - ' + self.nome
+class Cidade(models.model):
+    nome = models.charField(max_lenght=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
